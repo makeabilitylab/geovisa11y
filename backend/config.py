c68e0b6b081a7f87d40c9319f24bd1646b93ee8f
crescendochu <chuchuli@uw.edu>
2025-01-05 22:46:55 -0800

    Added local moran's I (spatial pattern) to the function

--- backend/config.py
+++ backend/config.py
@@ -9,4 +9,6 @@ class ProductionConfig(Config):
     DEBUG = False
 
 class DevelopmentConfig(Config):
+    DEBUG = True
+    OPENAI_API_KEY = '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'
     pass
